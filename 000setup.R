bdc60bd8ea6296cc4e6ecda50173ba07b850e99e
heelgueta <hermanelgueta@gmail.com>
2020-06-02 18:51:32 +0000

    avances subí 2 matrices de datos más

--- 000setup.R
+++ 000setup.R
@@ -1,35 +1,39 @@
+#aquí configuro el primer archivo
+
 ##setup and load data
+#rm(list = ls())
 options(scipen=999) #disables scientific notation
 options(max.print=1000000) #enable long outputs
-df5p00 <- read.csv("ide5p.csv")
-df5p00$pid <- rownames(df5p00)
+dfidm5p <- read.csv("idm5p.csv")
+dfidm7p <- read.csv("idm7p.csv")
+dfidc7p <- read.csv("idc7p.csv")
 
 ##cfa model testing
 #1 factor
-mod1f <- 'gen =~ id1 + id2 + id3 + id4 + id5 + id6 + id7 + id8 + id9'
-fit1f <- lavaan::cfa(mod1f, data=df5p00,estimator="MLR")
+mod1f <- 'gen =~ idm1 + idm2 + idm3 + idm4 + idm5 + idm6 + idm7 + idm8 + idm9'
+fit1f <- lavaan::cfa(mod1f, data=dfidm5p,estimator="MLR")
 #lavaan::summary(fit1f,standardized=TRUE)
 lavaan::fitMeasures(fit1f, c("chisq.scaled","df","pvalue.scaled","cfi.scaled","tli.scaled","rmsea.scaled"))
 lavaan::standardizedSolution(fit1f)
 
 #3 factors
-mod3f <- 'es1 =~ id1 + id2 + id3
-          es2 =~ id4 + id5 + id6
-          es3 =~ id7 + id8 + id9'
-fit3f <- lavaan::cfa(mod3f, data=df5p00,estimator="MLR")
+mod3f <- 'es1 =~ idm1 + idm2 + idm3
+          es2 =~ idm4 + idm5 + idm6
+          es3 =~ idm7 + idm8 + idm9'
+fit3f <- lavaan::cfa(mod3f, data=dfidm5p,estimator="MLR")
 #lavaan::summary(fit3f,standardized=TRUE)
 lavaan::fitMeasures(fit3f, c("chisq.scaled","df","pvalue.scaled","cfi.scaled","tli.scaled","rmsea.scaled"))
 lavaan::standardizedSolution(fit3f)
 
 #bifactor
-modbf <- 'gen =~ 1*id1 + 1*id2 + 1*id3 + 1*id4 + 1*id5 + 1*id6 + 1*id7 + 1*id8 + 1*id9
-          es1 =~ id1 + id2 + id3
-          es2 =~ id4 + id5 + id6
-          es3 =~ id7 + id8 + id9
+modbf <- 'gen =~ 1*idm1 + 1*idm2 + 1*idm3 + 1*idm4 + 1*idm5 + 1*idm6 + 1*idm7 + 1*idm8 + 1*idm9
+          es1 =~ idm1 + idm2 + idm3
+          es2 =~ idm4 + idm5 + idm6
+          es3 =~ idm7 + idm8 + idm9
           gen ~~ 0*es1
           gen ~~ 0*es2
           gen ~~ 0*es3'
-fitbf <- lavaan::cfa(modbf, data=df5p00,estimator="MLR")
+fitbf <- lavaan::cfa(modbf, data=dfidm5p,estimator="MLR")
 #lavaan::summary(fitbf,standardized=TRUE)
 lavaan::fitMeasures(fitbf, c("chisq.scaled","df","pvalue.scaled","cfi.scaled","tli.scaled","rmsea.scaled"))
 lavaan::standardizedSolution(fitbf)
@@ -47,8 +51,9 @@ boxplot(df5p01$idm~df5p01$sex)
 boxplot(df5p01$idm~df5p01$age)
 plot(df5p01$idm~df5p01$age)
 plot(jitter(df5p01$age),jitter(df5p01$idm))
+plot(jitter(df5p01$sex),jitter(df5p01$idm))
 
 hist(lavaan::lavPredict(fit1f))
 
-round(polycor::hetcor((df5p00[4:12]))$correlations,2)
-qgraph::qgraph(round(polycor::hetcor((df5p00[4:12]))$correlations,2),layout = "spring")
+round(polycor::hetcor((dfidm5p[4:12]))$correlations,2)
+qgraph::qgraph(round(polycor::hetcor((dfidm5p[4:12]))$correlations,2),layout = "spring")
